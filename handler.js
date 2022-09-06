c6407a8289874ac6fb37065feb2a47d606e976a9
Paulo Martins <paulosm.dev@gmail.com>
2022-09-06 00:00:17 -0300

    new_handler

--- handler.js
+++ handler.js
@@ -1,16 +1,25 @@
-"use strict";
+console.log('Loading function');
+        
+const aws = require('aws-sdk');
 
-module.exports.hello = async (event) => {
-  console.log(JSON.stringify('event', event))
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+exports.handler = async (event, context) => {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Bucket: bucket,
+        Key: key,
+    }; 
+    try {
+        const { ContentType } = await s3.getObject(params).promise();
+        console.log('CONTENT TYPE:', ContentType);
+        return ContentType;
+    } catch (err) {
+        console.log(err);
+        const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+        console.log(message);
+        throw new Error(message);
+    }
 };
+      
